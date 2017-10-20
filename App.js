bc8757c67950e38ef63ccaa821f3c02c1e7d7d5d
Mikazakaryan <Mikazakaryan94@gmail.com>
2017-10-21 01:34:59 +0400

    authentication

--- App.js
+++ App.js
@@ -40,3 +40,29 @@ const config = {
   messagingSenderId: "1087780000567"
 };
 firebase.initializeApp(config);
+
+// Listen for authentication state to change.
+firebase.auth().onAuthStateChanged((user) => {
+  if (user != null) {
+    console.log("We are authenticated now!");
+  }
+
+  // Do other things
+});
+
+async function loginWithFacebook() {
+  const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync(
+    '1911289432454064',
+    { permissions: ['public_profile'] }
+  );
+
+  if (type === 'success') {
+    // Build Firebase credential with the Facebook access token.
+    const credential = firebase.auth.FacebookAuthProvider.credential(token);
+
+    // Sign in with credential from the Facebook user.
+    firebase.auth().signInWithCredential(credential).catch((error) => {
+      // Handle Errors here.
+    });
+  }
+}
