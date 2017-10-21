91ed126e5c674e7bfa2e6cbdbf2edbac56dd8df1
Robert Adamyan <robert.adamyan1@gmail.com>
2017-10-21 11:35:14 +0400

    final cleanup

--- js/components/sideBar.js
+++ js/components/sideBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppRegistry, Image, StatusBar, StyleSheet } from "react-native";
+import { AppRegistry, Image, StatusBar, StyleSheet, View } from "react-native";
 import { Container, Content, Text, List, ListItem, Footer, FooterTab, Button, Right } from "native-base";
 const routes = ["Home", "Profile", "Marketplace", "Invite", "Exit"];
 
@@ -16,14 +16,23 @@ const styles = StyleSheet.create({
     color: '#fff'
   },
   footerHeader: {
-    color: '#82838f',
-    marginBottom: 7
+    color: '#82838f'
   },
   footerText: {
-    color: '#82838f',
+    color: '#82838f'
   },
   footer: {
     height: 300
+  },
+  name: {
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    marginBottom: 10,
+    marginLeft: 10
+  },
+  nameText: {
+    fontSize: 36
   }
 })
 
@@ -31,9 +40,9 @@ export default class SideBar extends React.Component {
   render() {
     return (
       <Container style={{
-        alignItems: 'flex-start',
-        justifyContent: 'center',
-        marginTop: 100,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        marginTop: 100
       }}>
         <Content>
           <List
