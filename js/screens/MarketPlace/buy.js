b3de5d30595cb5a32fc2514c58666032694215e1
adsellor <zaven.snkhchyan@gmail.com>
2017-10-21 08:33:55 +0400

    Change card styles

--- js/screens/MarketPlace/buy.js
+++ js/screens/MarketPlace/buy.js
@@ -7,10 +7,10 @@ class BuyScreen extends PureComponent {
     return (
       <Container>
         <Content>
-          <Card style={{flex: 0}}>
+          <Card style={{ flex: 0 }}>
             <CardItem>
               <Left>
-                <Thumbnail source={{uri: 'https://www.panorama.am/news_images/501/1500474_3/f5672ddd201d9e_5672ddd201de0.thumb.jpg'}} />
+                <Thumbnail source={{ uri: 'https://www.panorama.am/news_images/501/1500474_3/f5672ddd201d9e_5672ddd201de0.thumb.jpg' }} />
                 <Body>
                   <Text>Free Calls</Text>
                   <Text note>Untill November 15, 2017</Text>
@@ -19,17 +19,18 @@ class BuyScreen extends PureComponent {
             </CardItem>
             <CardItem>
               <Body>
-                <Image source={{uri: 'https://static.beeline.am/upload/images/Home-600-195x139.jpg'}} style={{height: 200, width: 200, flex: 1}}/>
                 <Text>
                   Get 100 Mins of Free Calls with 1000pts
                 </Text>
+                <Image source={{ uri: 'https://static.beeline.am/upload/images/Home-600-195x139.jpg' }} style={{ height: 200, width: 320, flex: 1 }} />
+
               </Body>
             </CardItem>
           </Card>
-          <Card style={{flex: 0}}>
+          <Card style={{ flex: 0 }}>
             <CardItem>
               <Left>
-                <Thumbnail source={{uri: 'https://www.panorama.am/news_images/501/1500474_3/f5672ddd201d9e_5672ddd201de0.thumb.jpg'}} />
+                <Thumbnail source={{ uri: 'https://www.panorama.am/news_images/501/1500474_3/f5672ddd201d9e_5672ddd201de0.thumb.jpg' }} />
                 <Body>
                   <Text>Free Text</Text>
                   <Text note>Untill November 15, 2017</Text>
@@ -38,17 +39,17 @@ class BuyScreen extends PureComponent {
             </CardItem>
             <CardItem>
               <Body>
-                <Image source={{uri: 'https://static.beeline.am/upload/dpcupload/contents/342/SMS-715x350.jpg'}} style={{height: 200, width: 200, flex: 1}}/>
                 <Text>
                   Get 25 free text messages With 500pts
                 </Text>
+                <Image source={{ uri: 'https://static.beeline.am/upload/dpcupload/contents/342/SMS-715x350.jpg' }} style={{ height: 200, width: 320, flex: 1 }} />
               </Body>
             </CardItem>
           </Card>
-          <Card style={{flex: 0}}>
+          <Card style={{ flex: 0 }}>
             <CardItem>
               <Left>
-                <Thumbnail source={{uri: 'https://www.panorama.am/news_images/501/1500474_3/f5672ddd201d9e_5672ddd201de0.thumb.jpg'}} />
+                <Thumbnail source={{ uri: 'https://www.panorama.am/news_images/501/1500474_3/f5672ddd201d9e_5672ddd201de0.thumb.jpg' }} />
                 <Body>
                   <Text>Free Data</Text>
                   <Text note>Untill November 30, 2017</Text>
@@ -57,15 +58,15 @@ class BuyScreen extends PureComponent {
             </CardItem>
             <CardItem>
               <Body>
-                <Image source={{uri: 'https://static.beeline.am/upload/dpcupload/contents/342/4G-LTE-715x350new.jpg'}} style={{height: 200, width: 200, flex: 1}}/>
                 <Text>
                   Get 2GB of free data with  800pts
                 </Text>
+                <Image source={{ uri: 'https://static.beeline.am/upload/dpcupload/contents/342/4G-LTE-715x350new.jpg' }} style={{ height: 200, width: 320, flex: 1 }} />
               </Body>
             </CardItem>
           </Card>
         </Content>
-        
+
       </Container>
     );
   }
