1e9382d24c842877c29b1c66d24c26536dec3038
adsellor <zaven.snkhchyan@gmail.com>
2017-10-21 10:28:46 +0400

    Add the grdonest thing I have ever seen: Mom, Dad sorry, I have dissapointed you

--- js/screens/MarketPlace/buy.js
+++ js/screens/MarketPlace/buy.js
@@ -1,12 +1,25 @@
 import React, { PureComponent } from 'react';
-import { Image } from 'react-native';
+import { Image, TouchableOpacity } from 'react-native';
+import Modal from 'react-native-modal';
+
 import { Container, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body } from 'native-base';
 
 class BuyScreen extends PureComponent {
+  state={
+    isVisible: false,
+    isVisible2: false,
+    isVisible3: false
+  }
+  _showModal = () => this.setState({ isVisible: true })
+  _showModal2 = () => this.setState({ isVisible2: true })
+  _showModal3 = () => this.setState({ isVisible3: true })
+  _hideModal = () => this.setState({ isVisible: false, isVisible2: false, isVisible3: false })
+  
   render() {
     return (
       <Container>
         <Content>
+        <TouchableOpacity onPress={this._showModal} >
           <Card style={{ flex: 0 }}>
             <CardItem>
               <Left>
@@ -27,6 +40,27 @@ class BuyScreen extends PureComponent {
               </Body>
             </CardItem>
           </Card>
+          </TouchableOpacity>
+          <Modal isVisible={this.state.isVisible}>
+          <CardItem onPress={this._hideModal}>
+              <Left>
+                <Thumbnail source={{ uri: 'https://www.panorama.am/news_images/501/1500474_3/f5672ddd201d9e_5672ddd201de0.thumb.jpg' }} />
+                <Body>
+                  <Text>Free Calls</Text>
+                  <Text note>Untill November 15, 2017</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>
+                  Get 100 Mins of Free Calls with 1000pts
+                </Text>
+                <Button onPress={this._hideModal} block primary><Text> Get </Text></Button>
+              </Body>
+            </CardItem>
+          </Modal>
+          <TouchableOpacity  onPress={this._showModal2}>
           <Card style={{ flex: 0 }}>
             <CardItem>
               <Left>
@@ -46,7 +80,28 @@ class BuyScreen extends PureComponent {
               </Body>
             </CardItem>
           </Card>
-          <Card style={{ flex: 0 }}>
+          </TouchableOpacity>
+          <Modal isVisible={this.state.isVisible2}>
+          <CardItem>
+              <Left>
+                <Thumbnail source={{ uri: 'https://www.panorama.am/news_images/501/1500474_3/f5672ddd201d9e_5672ddd201de0.thumb.jpg' }} />
+                <Body>
+                  <Text>Free Text</Text>
+                  <Text note>Untill November 15, 2017</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>
+                  Get 25 free text messages With 500pts
+                </Text>
+                <Button onPress={this._hideModal} block primary><Text> Get </Text></Button>
+              </Body>
+            </CardItem>
+            </Modal>
+            <TouchableOpacity onPress={this._showModal3}>
+          <Card  style={{ flex: 0 }}>
             <CardItem>
               <Left>
                 <Thumbnail source={{ uri: 'https://www.panorama.am/news_images/501/1500474_3/f5672ddd201d9e_5672ddd201de0.thumb.jpg' }} />
@@ -65,7 +120,27 @@ class BuyScreen extends PureComponent {
               </Body>
             </CardItem>
           </Card>
+          </TouchableOpacity>
         </Content>
+        <Modal isVisible={this.state.isVisible3}>
+          <CardItem>
+              <Left>
+                <Thumbnail source={{ uri: 'https://www.panorama.am/news_images/501/1500474_3/f5672ddd201d9e_5672ddd201de0.thumb.jpg' }} />
+                <Body>
+                  <Text>Free Data</Text>
+                  <Text note>Untill November 30, 2017</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>
+                  Get 2GB of free data with  800pts
+                </Text>
+                <Button onPress={this._hideModal} block primary><Text> Get </Text></Button>
+              </Body>
+            </CardItem>
+            </Modal>
 
       </Container>
     );
